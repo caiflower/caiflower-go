a17668b8f24241042b5c587099006a4e44690af3
lijinlong100 <lijinlong100@jd.com>
2025-09-26 17:34:20 +0800

    go-sse

--- pkg/go-see/client_test.go
+++ pkg/go-see/client_test.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"math/big"
 	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
-	"strings"
 	"sync"
 	"syscall"
 	"testing"
 
 	"github.com/tmaxmax/go-sse"
+)
+
+const (
+	EventTypeOfChatModelAnswer = "chat.answer"
+	EventTypeOfChatError       = "chat.error"
+	EventTypeOfChatFinish      = "chat.finish"
 )
 
 func TestSEE(t *testing.T) {
@@ -26,36 +30,33 @@ func TestSEE(t *testing.T) {
 	defer cancel()
 
 	wg := sync.WaitGroup{}
+	msg := ""
 	fn := func(userId string) {
 		defer wg.Done()
-		r, _ := http.NewRequestWithContext(ctx, http.MethodGet, getRequestURL(sub), http.NoBody)
+		r, _ := http.NewRequestWithContext(ctx, http.MethodGet, getRequestURL(sub, userId), http.NoBody)
 		r.Header.Set("X-User-Id", userId)
+		r.Header.Set("Last-Event-Id", "1")
 		conn := sse.NewConnection(r)
 
-		conn.SubscribeToAll(func(event sse.Event) {
-			switch event.Type {
-			case "cycles", "ops":
-				fmt.Printf("Metric %s: %s\n", event.Type, event.Data)
-			case "close":
-				fmt.Println("Server closed!")
+		conn.SubscribeToAll(func(ev sse.Event) {
+			switch ev.Type {
+			case EventTypeOfChatModelAnswer:
+				msg += ev.Data
+			case EventTypeOfChatError:
 				cancel()
-			default: // no event name
-				var sum, num big.Int
-				for _, n := range strings.Split(event.Data, "\n") {
-					_, _ = num.SetString(n, 10)
-					sum.Add(&sum, &num)
-				}
-
-				fmt.Printf("Sum of random numbers: %s\n", &sum)
+			case EventTypeOfChatFinish:
+				cancel()
 			}
 		})
 
 		if err := conn.Connect(); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
+
+		fmt.Println(msg)
 	}
 
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 1; i++ {
 		wg.Add(1)
 		go fn(fmt.Sprintf("user-%d", i))
 	}
@@ -63,17 +64,19 @@ func TestSEE(t *testing.T) {
 	wg.Wait()
 }
 
-func getRequestURL(sub string) string {
+func getRequestURL(sub string, sessionId string) string {
 	q := url.Values{}
 	switch sub {
 	case "all":
 		q.Add("topic", "numbers")
 		q.Add("topic", "metrics")
+
 	case "numbers", "metrics":
 		q.Set("topic", sub)
 	default:
 		panic(fmt.Errorf("unexpected subscription topic %q", sub))
 	}
+	q.Add("sessionId", sessionId)
 
-	return "http://localhost:8080/v1/agents:scheduling?" + q.Encode()
+	return "http://127.0.0.1:8080/v1/chat?input=%E4%BB%8B%E7%BB%8D%E4%B8%80%E4%B8%8B%E5%8C%97%E4%BA%AC&chatProtocol=ollama"
 }
